9d78de4eda0833b007ef3b46e00bfc2d8cc78d6d
Haoyu <ccc123zhy@outlook.com>
2026-07-24 12:09:27 +0800

    feat: 添加打印任务表，记录打印任务状态和相关信息

--- server/res/main.sql
+++ server/res/main.sql
@@ -103,6 +103,24 @@ CREATE TABLE IF NOT EXISTS settings(
     value TEXT NOT NULL
 )
 
+CREATE TABLE If NOT EXISTS print_task (
+    id TEXT PRIMARY KEY, -- uuid v7
+    status INTEGER DEFAULT 0，   -- 0：等待中 -- 1：打印中 -- 2：成功 --3：错误
+    
+    content TEXT NOT NULL, -- Receipt JSON
+    context TEXT, -- 上下文
+
+    error_message TEXT,
+
+    
+    created_at TIMESTAMP NOT NULL, -- 任务创建时间
+    started_at TIMESTAMP, -- 开始打印的时间
+    finished_at TIMESTAMP -- 打印完成时间（错误也用这个）
+
+    
+
+)
+
 -- command: users.new
 INSERT INTO users (username, password, is_admin, is_available, created_at)
 VALUES (?, ?, ?, ?, ?);
