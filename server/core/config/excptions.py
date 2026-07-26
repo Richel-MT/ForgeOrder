01730100a124b8808d0730612b29ff8f9a42e334
Haoyu <ccc123zhy@outlook.com>
2026-07-26 19:46:11 +0800

    feat: 重构 UnsupportedTypeError 异常类，更新错误消息以反映验证器的上下文

--- server/core/config/excptions.py
+++ server/core/config/excptions.py
@@ -1,19 +1,6 @@
-from math import exp
 
 
 
    
 
-class UnsupportedTypeError(Exception):
-    '''
-    该处理器无法处理这个值的类型。
-    '''
-    def __init__(self, verify_handler_class: type, expected_type: type, value_type: type):
-        self.verify_handler_class = verify_handler_class
-        self.expected_type = expected_type
-        self.expected_type = expected_type
-        self.value_type = value_type
 
-        super().__init__(
-            f"Handler '{self.verify_handler_class.__name__}' cannot deal with type {value_type}, expected type is {expected_type}."
-        )
--- server/core/validation/exceptions.py
+++ server/core/validation/exceptions.py
@@ -18,3 +18,17 @@ class ContextMissingError(Exception):
         self.context = context
 
         super().__init__("Context must provide a get() method, or be empty.")
+
+class UnsupportedTypeError(Exception):
+    '''
+    无法处理这个值的类型。
+    '''
+    def __init__(self, verify_handler_class: type, expected_type: type, value_type: type):
+        self.verify_handler_class = verify_handler_class
+        self.expected_type = expected_type
+        self.expected_type = expected_type
+        self.value_type = value_type
+
+        super().__init__(
+            f"Validator '{self.verify_handler_class.__name__}' does not support type {value_type}, expected type is {expected_type}."
+        )
