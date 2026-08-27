d5aeb511bb27d5cd6ed3950b769e8d35ee9e8eec
haoyu <ccc123zhy@outlook.com>
2026-08-27 14:04:26 +0800

    refactor: 移除冗余定义的异常类

--- server/core/validation/exceptions.py
+++ server/core/validation/exceptions.py
@@ -1,23 +1,5 @@
 from typing import Any
 
-class UnsupportedValidatorError(Exception):
-
-    '''
-    使用FunctionHandler，函数的返回值结构不正确
-    '''
-    def __init__(self, validatorClass: type):
-        self.validatorClass = validatorClass
-        
-        super().__init__(f"Implemented {validatorClass.__name__} in an unsupported way.")
-
-
-
-class ContextMissingError(Exception):
-
-    def __init__(self, context: Any):
-        self.context = context
-
-        super().__init__("Context must provide a get() method, or be empty.")
 
 class UnsupportedTypeError(Exception):
     '''
@@ -33,14 +15,3 @@ class UnsupportedTypeError(Exception):
             f"Validator ' {self.validatorClass.__name__}' does not support type {self.valueType}, expected type is {expectedType}."
         )
 
-class NotIterableError(Exception):
-    '''
-    传入的值不是可迭代对象
-    '''
-    def __init__(self, validatorClass: type, valueType: type):
-        self.validatorClass = validatorClass
-        self.valueType = valueType
-
-        super().__init__(
-            f"Validator ' {self.validatorClass.__name__}' requires an iterable value, but got {self.valueType}."
-        )
